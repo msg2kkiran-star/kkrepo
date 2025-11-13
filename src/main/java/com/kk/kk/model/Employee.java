64444713bce406515cf86399438522d490133671
k.gurubasu.kumbhar <msg2kkiran@gmail.com>
2025-11-14 01:40:10 +0530

    MVC validation annotations

--- src/main/java/com/kk/kk/model/Employee.java
+++ src/main/java/com/kk/kk/model/Employee.java
@@ -1,8 +1,15 @@
 package com.kk.kk.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class Employee {
+    @NotNull(message = "id can not be empty")
     private int id;
+
     private String name;
+
+    @Min(message = "No employee without salary", value = 1)
     private double salary;
 
     public Employee() {
